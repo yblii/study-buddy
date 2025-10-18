57a71b437bc4782198aa4e4fee4ee29f2b0171d7
yblii <yb.li0919@gmail.com>
2025-10-18 15:53:48 -0700

    Improved chat UI and added scrolling.

--- frontend/src/components/MessageList.js
+++ frontend/src/components/MessageList.js
@@ -2,7 +2,7 @@ import { TextBubble } from "./TextBubble";
 
 export function MessageList(props) {
     return (
-        <div className="message-list">
+        <div className="flex-col pb-3 w-full h-4/5">
             {props.messages.map((msg) => (
                 <TextBubble text={msg.parts[0]} 
                     isUser={msg.role === "user" ? true : false} 
--- frontend/src/components/TextBubble.js
+++ frontend/src/components/TextBubble.js
@@ -1,4 +1,13 @@
 export function TextBubble(props) {
+    let classes = "";
+    if(props.isUser) {
+        classes = "justify-self-end bg-primary text-right"; 
+    } else {
+        classes = "justify-self-start bg-white text-left"; 
+    }
+
+    classes += " rounded-md p-1 pl-2 pr-2";
+
     return (
         <div>
             <p className={props.isUser ? "user-msg" : "bot-msg"}>
