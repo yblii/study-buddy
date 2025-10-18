3dc1dcf587164f92e98a8da89d5cdc396f111826
nyctikorax <lw6561@gmail.com>
2025-10-18 13:34:37 -0700

    index.js in progress (temp with history functionality)

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,35 @@ import express from 'express';
 import cors from 'cors';
 import {GoogleGenAI} from '@google/genai';
 import Dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const ai = new GoogleGenAI({apiKey:GEMINI_API_KEY});
+const model = ai.getGenerativeModel({model : "gemini-2.5-flash"});
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello, World!" });
+});
+
+app.post("/api/chat", async (req, res) => {
+    try {
+        const {chatHistory, userMessage} = req.body;
+
+        chatHistory.push({
+            role: "user",
+            parts: [{ text: userMessage }]
+        });
+
+        const chat = model.startChat({
+            history: chatHistory
+        });
+
+        const result = await chat.sendMessage(userMessage);
+    }
 });
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
