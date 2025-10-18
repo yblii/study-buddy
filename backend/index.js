4239da70f78aeba8b2684aa02087ff1cfcad907b
nyctikorax <lw6561@gmail.com>
2025-10-18 14:03:35 -0700

    Working test 1

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const ai = new GoogleGenAI({apiKey:GEMINI_API_KEY});
+const ai = new GoogleGenAI();
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello, World!" });
+});
+
+app.get("/api/chat", async (req, res) => {
+  const message = req.query.message;
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: message,
+    thinkingBudget: 0,
+  });
+  res.send(response.text);
+  console.log(response.text);
 });
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
