5a9f08f1bf676e5ff6624e48c45da398a162f4e9
yblii <yb.li0919@gmail.com>
2025-10-18 14:46:02 -0700

    Added dotenv to keep api key secure

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import {GoogleGenAI} from '@google/genai';
-import Dotenv from 'dotenv';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const ai = new GoogleGenAI({apiKey : 'AIzaSyAY7wcjx3HanAkWKEjEI5jasoUFs9HRpvk'});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello, World!" });
