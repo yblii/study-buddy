947d883ae34c2d283c24a2e63abcd0876a82286c
nyctikorax <lw6561@gmail.com>
2025-10-18 12:48:46 -0700

    Update index.js
    Add imports for .env api use backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import {GoogleGenAI} from '@google/genai';
+import Dotenv from 'dotenv';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const ai = new GoogleGenAI({apiKey:GEMINI_API_KEY});
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello, World!" });
