4599047f2b22a4cfff77cab52a4e150f157074af
yblii <yb.li0919@gmail.com>
2025-10-18 22:08:12 -0700

    Improved UI

--- frontend/src/components/chat_page/ChatPage.js
+++ frontend/src/components/chat_page/ChatPage.js
@@ -6,15 +6,18 @@ export function ChatPage() {
     const [chatHistory, setChatHistory] = React.useState();
 
     return (
-        <div className="bg-bg bg-repeat bg-contain h-screen flex items-center 
-                justify-center">
-            <ChatWindow setHistory={setChatHistory}/>
-            <div className='backdrop-saturate-[.85]  absolute top-0 left-0 w-full h-full'></div>
+        <div className="p-6 w-screen h-screen grid grid-cols-5 items-center">
+            <div class="col-span-2">
 
-            <Link to="/analytics" className="absolute top-4 right-4 bg-white bg-opacity-70 hover:bg-opacity-90 
-                text-gray-800 font-semibold py-2 px-4 rounded shadow" state={{ history: chatHistory }}>
-                Analytics
-            </Link>
+            </div>
+            <div class="col-span-3">
+                <ChatWindow setHistory={setChatHistory}/>
+                
+                <Link to="/analytics" className="absolute top-4 right-4 bg-white bg-opacity-70 hover:bg-opacity-90 
+                    text-gray-800 font-semibold py-2 px-4 rounded shadow" state={{ history: chatHistory }}>
+                    Analytics
+                </Link>
+            </div>
         </div>
     );
 }
