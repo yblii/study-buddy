ff294fadb5191aa36221c695bee5dd3e79b00776
yunsophie <yunsophieli@gmail.com>
2025-10-19 03:10:14 -0700

    firebase init

---
+++ frontend/src/firebase/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+}
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
