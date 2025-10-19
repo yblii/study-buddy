c64a8e8d48883259dd2467b0eee9e5a9f2edc5e0
yblii <yb.li0919@gmail.com>
2025-10-19 15:37:03 -0700

    Fixed port for remote hosting.

--- backend/index.js
+++ backend/index.js
@@ -150,4 +150,4 @@ app.post("/api/analyze", async (req, res) => {
  });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log("Server running on http://localhost:8080"));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
